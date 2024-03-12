2267da4c645037815b50b67fbfed3238dd3da1fa
Cody Bashaw <148400196+C-Bashaw@users.noreply.github.com>
2024-03-12 11:28:45 -0500

    sort letters by number of appearance

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_letters = get_num_letters(text)
-    print(f"Words: {num_words} ")
-    print(num_letters)
+    print(f"Begin Report")
+    print(f"{num_words} words found in the document")
+    for letter in num_letters:
+        print(f"The {letter['char']} character was found {letter['count']} times")
 
 def get_num_words(text):
     words = text.split()
@@ -19,8 +21,15 @@ def get_num_letters(text):
     lower_text = text.lower()
     for c in lower_text:
         if c.isalpha():
-            letters[c] = (letters.get(c, 0)) + 1      
-    return {k: letters[k] for k in sorted(letters)}
+            letters[c] = (letters.get(c, 0)) + 1
+    list_of_letters = []
+    for l, count in letters.items():
+        new_list = {'char': l, 'count': count}
+        list_of_letters.append(new_list)
+    list_of_letters.sort(reverse=True, key=get_count)
+    return list_of_letters
 
+def get_count(letters):
+    return letters['count']
 
 main()
